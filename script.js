8c88470d2cbcdeeee9fa4d2f7e0702371df63200
Gimsion <si5ni@naver.com>
2026-02-10 18:18:45 +0900

    Update script.js

--- script.js
+++ script.js
@@ -5,8 +5,9 @@ const sentences = [
   "오늘의 문장이 내일의 나를 만든다."
 ];
 
-function pickSentence() {
+function picksentence() {
   const randomIndex = Math.floor(Math.random() * sentences.length);
   document.getElementById("result").innerText = sentences[randomIndex];
 }
+
 
